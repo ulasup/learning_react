107ccd3ff7528c29269e82adf44c7c8614ffe9d1
Urszula Suplicka <usuplicka@gmail.com>
2021-03-14 20:50:45 +0100

    Added new filter to getCardsForColumn

--- src/redux/cardsRedux.js
+++ src/redux/cardsRedux.js
@@ -1,7 +1,7 @@
 import shortid from 'shortid';
 
 // selectors
-export const getCardsForList = ({cards}, columnId) => cards.filter(card => card.columnId == columnId);
+export const getCardsForList = ({cards, searchString}, columnId) => cards.filter(card => card.columnId == columnId && new RegExp(searchString, 'i').test(card.title) == true);
 
 // action name creator
 const reducerName = 'cards';
