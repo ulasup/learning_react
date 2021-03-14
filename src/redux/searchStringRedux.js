8bc190b4927b82de31e3c046f4eec944712674a8
Urszula Suplicka <usuplicka@gmail.com>
2021-03-14 20:59:00 +0100

    Problem with searchStringRedux

--- src/redux/searchStringRedux.js
+++ src/redux/searchStringRedux.js
@@ -6,14 +6,20 @@ export const countAllCards = ({cards}) => cards.length;
 export const countVisibleCards = ({cards, searchString}) => cards.filter(card => new RegExp(searchString, 'i').test(card.title)).length;
 
 // action name creator
+const reducerName = 'searchString';
+const createActionName = name => `app/${reducerName}/${name}`;
 
 // actions types
+export const ADD_CHANGE = createActionName('ADD_CHANGE');
 
 // action creators
+// export const createActionAddChange = payload => ({ payload: { ...payload, id: shortid.generate() }, type: ADD_CHANGE });
 
 // reducer
 export default function reducer(statePart = '', action = {}) {
   switch (action.type) {
+    case ADD_CHANGE:
+      return payload; // action.payload?
     default:
       return statePart;
   }
