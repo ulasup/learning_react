4c6b4cbca716939e7b21d2c8cc8f6edef55ceec2
Urszula Suplicka <usuplicka@gmail.com>
2021-03-16 18:42:19 +0100

    Added MainLayout and navigation

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import Home from '../Home/HomeContainer';
+import Info from '../Info/Info';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import MainLayout from '../MainLayout/MainLayout.js';
 
 const App = () => (
-  <div>
-    <Home />
-  </div>
+  <BrowserRouter>
+    <MainLayout>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/info' component={Info} />
+      </Switch>
+    </MainLayout>
+  </BrowserRouter>
 );
 
 export default App;
