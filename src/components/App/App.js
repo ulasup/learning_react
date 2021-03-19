c8b51a15a0db114fbebe0f664fa1dc55eb54406a
Urszula Suplicka <usuplicka@gmail.com>
2021-03-19 13:19:17 +0100

    Added AnimatedSwitch to App js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import Home from '../Home/HomeContainer';
 import Info from '../Info/Info';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
 import MainLayout from '../MainLayout/MainLayout.js';
 import FAQ from '../FAQ/FAQ';
+import {AnimatedSwitch} from 'react-router-transition';
 
 const App = () => (
   <BrowserRouter>
     <MainLayout>
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/info' component={Info} />
+      <AnimatedSwitch
+        atEnter={{ opacity: 0 }}
+        atLeave={{ opacity: 0 }}
+        atActive={{ opacity: 1 }}
+        className="switch-wrapper"
+      >
+        <Route exact path="/" component={Home} />
+        <Route exact path="/info" component={Info} />
         <Route exact path='/faq' component={FAQ} />
-      </Switch>
+      </AnimatedSwitch>
     </MainLayout>
   </BrowserRouter>
 );
